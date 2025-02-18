77df389c498fbd7223fe3be7d19de665a1f5187d
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-02-18 17:33:38 +0300

    bakacam.

---
+++ KeycloakExtended/Services/KeyCloakService.cs
@@ -0,0 +1,12 @@
+﻿namespace KeycloakExtended.Services
+{
+    public sealed class KeyCloakService
+    {
+        public async  Task<string> GetAccessToken()
+        {
+            HttpClient client = new();
+            string endpoint = "http://localhost:8080/realms/myrealm/protocol/openid-connect/token";
+
+        }
+    }
+}
